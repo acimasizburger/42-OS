076057f389032a9a5ebe773dfcccc69b76ab8b16
Alperen Turan <146375395+acimasizburger@users.noreply.github.com>
2026-05-17 23:51:52 +0300

    Login ekranı eklendi

---
+++ os-protectors/scripts/login_manager.gd
@@ -0,0 +1,12 @@
+extends Control
+
+@export var github_url: String = "https://github.com/acimasizburger/42-OS"
+
+const Main = preload("res://scenes/main.tscn")
+
+func _on_github_button_pressed() -> void:
+	OS.shell_open(github_url)
+
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_packed(Main)
